cae880c64fa6eb0ae657e1057382f24d068ed100
FreshMag <pescomagnani@gmail.com>
2024-11-13 11:35:39 +0100

    feat: added trys in the reader

--- generator/src/main/kotlin/io/github/subjekt/files/Reader.kt
+++ generator/src/main/kotlin/io/github/subjekt/files/Reader.kt
@@ -10,12 +10,16 @@ object Reader {
   private val mapper = ObjectMapper(YAMLFactory()).apply {
     findAndRegisterModules()
   }
-  fun suiteFromYaml(path: File): Suite =
-    mapper.readValue(path)
 
-  fun suiteFromResource(path: String): Suite {
-    val file = Reader::class.java.classLoader.getResource(path)?.path?.let { File(it) }
-      ?: throw IllegalArgumentException("Resource not found: $path")
+  fun suiteFromYaml(path: File): Suite? =
+    try { mapper.readValue(path) } catch (e: Exception) { println("There was an error: ${e.message}"); null }
+
+  fun suiteFromYaml(yaml: String): Suite? =
+    try { mapper.readValue(yaml) } catch (e: Exception) { println("There was an error; ${e.message}"); null }
+
+  fun suiteFromResource(path: String): Suite? {
+    val file = Reader::class.java.classLoader.getResource(path)?.path?.let { File(it) } ?: return null
     return suiteFromYaml(file)
   }
+
 }
