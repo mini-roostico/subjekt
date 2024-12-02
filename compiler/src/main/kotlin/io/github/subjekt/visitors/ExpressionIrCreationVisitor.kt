1c8a010f6a60e0067f65ac72e26877c4ca39371b
FreshMag <pescomagnani@gmail.com>
2024-12-02 17:06:26 +0100

    feat: implementation of ExpressionIrCreationVisitor.kt

--- compiler/src/main/kotlin/io/github/subjekt/visitors/ExpressionIrCreationVisitor.kt
+++ compiler/src/main/kotlin/io/github/subjekt/visitors/ExpressionIrCreationVisitor.kt
@@ -2,28 +2,52 @@ package io.github.subjekt.visitors
 
 import io.github.subjekt.ExpressionBaseVisitor
 import io.github.subjekt.ExpressionParser
-import io.github.subjekt.nodes.Context
 import io.github.subjekt.nodes.expression.Node
+import io.github.subjekt.utils.MessageCollector
+import org.antlr.v4.runtime.ParserRuleContext
 
-class ExpressionIrCreationVisitor(context: Context) : ExpressionBaseVisitor<Node>() {
+class ExpressionIrCreationVisitor(val messageCollector: MessageCollector) : ExpressionBaseVisitor<Node>() {
 
-  override fun visitCall(ctx: ExpressionParser.CallContext?): Node? {
-    return super.visitCall(ctx)
+  private fun ParserRuleContext.createError(message: String) {
+    messageCollector.error("Line ${this.start.line}: $message")
   }
 
-  override fun visitVariable(ctx: ExpressionParser.VariableContext?): Node? {
-    return super.visitVariable(ctx)
+  override fun visitCall(ctx: ExpressionParser.CallContext): Node? =
+    visit(ctx.macroCall())
+
+  override fun visitVariable(ctx: ExpressionParser.VariableContext): Node? =
+    Node.Id(ctx.text)
+
+  override fun visitPlusExpr(ctx: ExpressionParser.PlusExprContext): Node? {
+    val left = visit(ctx.expression(0))
+    val right = visit(ctx.expression(1))
+    if (left == null) {
+      ctx.createError("Left side of plus expression is null")
+      return null
+    }
+    if (right == null) {
+      ctx.createError("right side of plus expression is null")
+      return null
+    }
+    return Node.Plus(
+      visit(ctx.expression(0)),
+      visit(ctx.expression(1))
+    )
   }
 
-  override fun visitPlusExpr(ctx: ExpressionParser.PlusExprContext?): Node? {
-    return super.visitPlusExpr(ctx)
-  }
+  override fun visitLiteral(ctx: ExpressionParser.LiteralContext): Node? =
+    Node.Literal(ctx.text.trim().removePrefix("\"").removeSuffix("\""))
 
-  override fun visitLiteral(ctx: ExpressionParser.LiteralContext?): Node? {
-    return super.visitLiteral(ctx)
-  }
-
-  override fun visitMacroCall(ctx: ExpressionParser.MacroCallContext?): Node? {
-    return super.visitMacroCall(ctx)
+  override fun visitMacroCall(ctx: ExpressionParser.MacroCallContext): Node? {
+    val id = ctx.ID()?.text
+    if (id == null) {
+      ctx.createError("macro call has no identifier")
+      return null
+    }
+    val arguments = ctx.expression().map { visit(it) }
+    return Node.Call(
+      id,
+      arguments
+    )
   }
 }
