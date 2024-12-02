06934a95bd2e5d921c72cb700ed5258f9c4aec58
FreshMag <pescomagnani@gmail.com>
2024-12-02 18:43:13 +0100

    refactor: added message collector to Resolvable methods

--- compiler/src/main/kotlin/io/github/subjekt/resolved/Resolvable.kt
+++ compiler/src/main/kotlin/io/github/subjekt/resolved/Resolvable.kt
@@ -1,10 +1,11 @@
 package io.github.subjekt.resolved
 
 import io.github.subjekt.nodes.Context
+import io.github.subjekt.utils.MessageCollector
 
 interface Resolvable {
 
-  fun resolveOne(context: Context): String
+  fun resolveOne(context: Context, messageCollector: MessageCollector): String
 
-  fun resolve(context: Context): Iterable<String>
+  fun resolve(context: Context, messageCollector: MessageCollector): Iterable<String>
 }
