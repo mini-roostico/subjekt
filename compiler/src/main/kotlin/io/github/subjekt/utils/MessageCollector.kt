f74a4efc66165a91c8a9b02c3e4ec484e9300116
FreshMag <pescomagnani@gmail.com>
2024-12-02 17:06:15 +0100

    feat: implementation of a simple MessageCollector to collect messages

--- compiler/src/main/kotlin/io/github/subjekt/utils/MessageCollector.kt
+++ compiler/src/main/kotlin/io/github/subjekt/utils/MessageCollector.kt
@@ -1,6 +1,71 @@
 package io.github.subjekt.utils
 
+import org.antlr.v4.runtime.BaseErrorListener
+import org.antlr.v4.runtime.ConsoleErrorListener
+import org.antlr.v4.runtime.Parser
+import org.antlr.v4.runtime.RecognitionException
+import org.antlr.v4.runtime.Recognizer
+
 sealed class MessageCollector {
 
-  class NullCollector : MessageCollector()
+  enum class MessageType {
+    INFO,
+    WARNING,
+    ERROR
+  }
+
+  abstract val messages: List<Message>
+
+  abstract fun info(message: String)
+  abstract fun warning(message: String)
+  abstract fun error(message: String)
+  abstract fun flushMessages()
+
+  data class Message(val type: MessageType, val message: String)
+
+  fun useParser(parser: Parser) {
+    parser.removeErrorListener(ConsoleErrorListener.INSTANCE)
+    parser.addErrorListener(object : BaseErrorListener() {
+      override fun syntaxError(
+        recognizer: Recognizer<*, *>?,
+        offendingSymbol: Any?,
+        line: Int,
+        charPositionInLine: Int,
+        msg: String?,
+        e: RecognitionException?
+      ) {
+        error("Line $line:$charPositionInLine $msg")
+      }
+
+    })
+  }
+
+  class SimpleCollector : MessageCollector() {
+    override var messages = emptyList<Message>()
+      private set
+
+    override fun info(message: String) {
+      messages += Message(MessageType.INFO, message)
+    }
+
+    override fun warning(message: String) {
+      messages += Message(MessageType.WARNING, message)
+    }
+
+    override fun error(message: String) {
+      messages += Message(MessageType.ERROR, message)
+    }
+
+    override fun flushMessages() {
+      messages = emptyList()
+    }
+  }
+
+  class NullCollector : MessageCollector() {
+    override val messages = emptyList<Message>()
+    override fun info(message: String) {}
+    override fun warning(message: String) {}
+    override fun error(message: String) {}
+    override fun flushMessages() {}
+  }
 }
