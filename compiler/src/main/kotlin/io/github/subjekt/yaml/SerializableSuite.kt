60aaf2f9cdea8e077b23b1401ee749ce382b0912
FreshMag <pescomagnani@gmail.com>
2024-12-02 15:05:43 +0100

    feat: moved yaml serialization files

---
+++ compiler/src/main/kotlin/io/github/subjekt/yaml/SerializableSuite.kt
@@ -0,0 +1,27 @@
+package io.github.subjekt.yaml
+
+data class Suite(
+  val name: String,
+  val config: Configuration?,
+  val macros: List<Macro>?,
+  val subjects: List<Subject>,
+  val parameters: List<Parameter>?,
+)
+
+data class Subject(
+  val name: String,
+  val parameters: List<Parameter>?,
+  val macros: List<Macro>?,
+  val code: String,
+  val outcomes: List<Outcome>
+)
+
+data class Macro(val def: String, val values: List<String>)
+
+data class Parameter(val name: String, val values: List<Any>)
+
+data class Outcome(val warning: String?, val error: String?)
+
+data class Configuration(
+  val codePreamble: String = "",
+)
