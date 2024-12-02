ecc6f91e70b4bc94c35c358be43ef4c8eede8281
FreshMag <pescomagnani@gmail.com>
2024-12-02 15:07:42 +0100

    feat: added node classes first implementation

---
+++ compiler/src/main/kotlin/io/github/subjekt/nodes/Context.kt
@@ -0,0 +1,49 @@
+package io.github.subjekt.nodes
+
+import io.github.subjekt.utils.MessageCollector
+
+class Context(private val collector: MessageCollector) {
+
+  val parameter = mutableMapOf<String, Any>()
+  private val macros = mutableMapOf<String, Macro>()
+
+  fun parameterSnapshot(): Map<String, Any> =
+    parameter.toMap()
+
+  fun macroSnapshot(): Map<String, Macro> =
+    macros.toMap()
+
+  operator fun plus(pairs: Context): Context {
+    TODO()
+  }
+
+  fun putParameter(identifier: String, value: Any) {
+    parameter[identifier] = value
+  }
+
+  fun putMacro(macro: Macro) {
+    macros[macro.identifier] = macro
+  }
+
+  fun warning(message: String) {
+    TODO()
+  }
+
+  fun error(message: String) {
+    TODO()
+  }
+
+
+  companion object {
+    fun emptyContext(): Context {
+      return Context(MessageCollector.NullCollector())
+    }
+
+    fun of(
+      vararg parameters: Pair<String, Any>,
+      messageCollector: MessageCollector = MessageCollector.NullCollector()
+    ) = Context(messageCollector).also { context ->
+      parameters.forEach { context.putParameter(it.first, it.second) }
+    }
+  }
+}
---
+++ compiler/src/main/kotlin/io/github/subjekt/nodes/Parameter.kt
@@ -0,0 +1,13 @@
+package io.github.subjekt.nodes
+
+data class Parameter(
+  val name: String,
+  val values: List<Any>,
+) {
+
+  companion object {
+    fun fromYamlParameter(parameter: io.github.subjekt.yaml.Parameter): Parameter {
+      return Parameter(parameter.name, parameter.values)
+    }
+  }
+}
