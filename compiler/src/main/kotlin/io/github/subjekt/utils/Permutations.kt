52d6b3d9f64ad1020d65ddf2349095054241dcaa
FreshMag <pescomagnani@gmail.com>
2024-12-02 15:07:56 +0100

    feat: added permutations utilities

---
+++ compiler/src/main/kotlin/io/github/subjekt/utils/Permutations.kt
@@ -0,0 +1,38 @@
+package io.github.subjekt.utils
+
+import io.github.subjekt.nodes.Parameter
+
+object Permutations {
+
+  data class DefinedParameter(
+    val identifier: String,
+    val value: Any
+  )
+
+  fun List<Parameter>.permute(parameterConfigurationConsumer: (List<DefinedParameter>) -> Unit) {
+    val cartesianProduct = this.map { it.values }.fold(sequenceOf(emptyList<Any>())) { acc, values ->
+      acc.flatMap { combination ->
+        values.asSequence().map { combination + it }
+      }
+    }
+
+    cartesianProduct.map { combination ->
+      combination.mapIndexed { index, value ->
+        DefinedParameter(this[index].name, value)
+      }
+    }.forEach {
+        parameterConfigurationConsumer(it)
+    }
+  }
+
+  fun <T> Iterable<Iterable<T>>.permute(): Iterable<Iterable<T>> {
+    return fold(listOf(emptyList<T>()) as Iterable<List<T>>) { acc, iterable ->
+      acc.flatMap { combination ->
+        iterable.map { element ->
+          combination + element
+        }
+      }
+    }
+  }
+
+}
