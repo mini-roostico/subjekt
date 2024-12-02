7b0eaaa5918b945ca9378a3d7ecc00fe996b096c
FreshMag <pescomagnani@gmail.com>
2024-12-02 20:40:10 +0100

    test: added more tests for expression evaluation

--- compiler/src/test/kotlin/io/github/subjekt/visitors/ExpressionEvaluationTest.kt
+++ compiler/src/test/kotlin/io/github/subjekt/visitors/ExpressionEvaluationTest.kt
@@ -5,9 +5,11 @@ import io.github.subjekt.nodes.suite.Macro
 import io.github.subjekt.nodes.suite.Template
 import io.github.subjekt.utils.Expressions.evaluate
 import io.github.subjekt.utils.MessageCollector
+import io.github.subjekt.utils.MessageCollector.Message
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import kotlin.test.assertContains
 import kotlin.test.assertEquals
 
 class ExpressionEvaluationTest {
@@ -79,6 +81,74 @@ class ExpressionEvaluationTest {
         "{{value2}}",
       ),
       result.toSet(),
+    )
+  }
+
+  @Test
+  fun `Expression evaluation with nested calls and literals`() {
+    val expr = "bar(\"1\" + \"2\")"
+    val result = expr.evaluate(context, collector)
+    assertEquals(listOf("(12)", "{12}"), result)
+  }
+
+  @Test
+  fun `Expression evaluation with nested calls and literals and arguments`() {
+    val expr = "bar(\"1\" + \"2\" + a)"
+    val result = expr.evaluate(context, collector)
+    assertEquals(listOf("(121)", "{121}"), result)
+  }
+
+  @Test
+  fun `Expression evaluation with nested calls and literals and arguments and multiple calls`() {
+    val expr = "bar(\"1\" + \"2\" + a) + bar(\"3\" + \"4\" + b)"
+    val result = expr.evaluate(context, collector)
+    assertEquals(setOf("(121)(342)", "(121){342}", "{121}(342)", "{121}{342}"), result.toSet())
+  }
+
+  @Test
+  fun `Expression with newline`() {
+    context.putMacro(
+      Macro(
+        "aligned",
+        listOf("code"),
+        listOf(Template.parse("alignedOn(0) {\n\t\${{code }}\n}")),
+      ),
+    )
+    val expr = "aligned(\"exampleCall(123)\")"
+    val result = expr.evaluate(context, collector)
+    assertEquals(listOf("alignedOn(0) {\n\texampleCall(123)\n}"), result)
+  }
+
+  @Test
+  fun `ID not defined`() {
+    val expr = "c"
+    val result = expr.evaluate(context, collector)
+    assertEquals(emptyList(), result)
+    assertContains(
+      collector.messages,
+      Message(MessageCollector.MessageType.ERROR, "Line 1: Identifier 'c' is not defined"),
+    )
+  }
+
+  @Test
+  fun `Macro not defined`() {
+    val expr = "baz()"
+    val result = expr.evaluate(context, collector)
+    assertEquals(emptyList(), result)
+    assertContains(
+      collector.messages,
+      Message(MessageCollector.MessageType.ERROR, "Line 1: Macro 'baz' is not defined"),
+    )
+  }
+
+  @Test
+  fun `Macro with wrong number of arguments`() {
+    val expr = "bar()"
+    val result = expr.evaluate(context, collector)
+    assertEquals(emptyList(), result)
+    assertContains(
+      collector.messages,
+      Message(MessageCollector.MessageType.ERROR, "Line 1: Macro 'bar' expects 1 arguments, but got 0"),
     )
   }
 }
