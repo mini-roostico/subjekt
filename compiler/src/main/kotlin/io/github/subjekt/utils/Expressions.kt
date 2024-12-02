072f24334ff9d2e406008968470a5d14de354d20
FreshMag <pescomagnani@gmail.com>
2024-12-02 19:09:51 +0100

    feat: added first implementation of ExpressionResolveVisitor.kt

--- compiler/src/main/kotlin/io/github/subjekt/utils/Expressions.kt
+++ compiler/src/main/kotlin/io/github/subjekt/utils/Expressions.kt
@@ -1,12 +1,28 @@
 package io.github.subjekt.utils
 
+import io.github.subjekt.ExpressionLexer
+import io.github.subjekt.ExpressionParser
 import io.github.subjekt.nodes.Context
+import io.github.subjekt.visitors.ExpressionIrCreationVisitor
+import io.github.subjekt.visitors.ExpressionResolveVisitor
+import org.antlr.v4.runtime.CharStreams
+import org.antlr.v4.runtime.CommonTokenStream
 
 object Expressions {
 
-  fun String.evaluate(context: Context): List<String> =
-    listOf(context.parameterSnapshot().entries.fold(this) { acc, entry ->
-      acc.replace(entry.key, entry.value.toString())
-      TODO()
-    })
+  fun String.evaluate(context: Context, messageCollector: MessageCollector): List<String> {
+    val stream = CharStreams.fromString(this)
+    val lexer = ExpressionLexer(stream)
+    messageCollector.useLexer(lexer)
+    val tokens = CommonTokenStream(lexer)
+    val parser = ExpressionParser(tokens)
+    messageCollector.useParser(parser)
+    val tree = parser.expression()
+    if (parser.numberOfSyntaxErrors > 0) {
+      return emptyList()
+    }
+    val ast = ExpressionIrCreationVisitor(messageCollector).visit(tree)
+    return ExpressionResolveVisitor(context, messageCollector).visit(ast)
+  }
+
 }
