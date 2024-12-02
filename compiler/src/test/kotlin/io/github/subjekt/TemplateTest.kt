3328160500687249ee458f7ab1bd5524535b6096
FreshMag <pescomagnani@gmail.com>
2024-12-02 15:08:57 +0100

    test: added first tests for template resolution and parsing

---
+++ compiler/src/test/kotlin/io/github/subjekt/TemplateTest.kt
@@ -0,0 +1,37 @@
+package io.github.subjekt
+
+import io.github.subjekt.nodes.Context
+import io.github.subjekt.nodes.Template
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class TemplateTest {
+
+  @Test
+  fun `Simple template parsing`() {
+    val templateString = "Hello, \${{ name }}!"
+    val template = Template.parse(templateString)
+    val expected = Template("Hello, %s!", listOf("name"))
+    assertEquals(expected, template)
+  }
+
+  @Test
+  fun `Simple template resolution`() {
+    val templateString = "Hello, \${{ name }}!"
+    val template = Template.parse(templateString)
+    val context = Context.of("name" to "World")
+    val resolved = template.resolve(context)
+    val expected = listOf("Hello, World!")
+    assertEquals(expected, resolved)
+  }
+
+  @Test
+  fun `Multiple expressions template resolution`() {
+    val templateString = "Hello, \${{ name }}! I'm \${{ age }} years old."
+    val template = Template.parse(templateString)
+    val context = Context.of("name" to "World", "age" to 42)
+    val resolved = template.resolve(context)
+    val expected = listOf("Hello, World! I'm 42 years old.")
+    assertEquals(expected, resolved)
+  }
+}
