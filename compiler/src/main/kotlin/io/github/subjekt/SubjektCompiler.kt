25455a85703936dcb731acc5938da536efc270b5
FreshMag <pescomagnani@gmail.com>
2024-12-03 16:26:48 +0100

    feat: added entry points for the compiler

--- compiler/src/main/kotlin/io/github/subjekt/SubjektCompiler.kt
+++ compiler/src/main/kotlin/io/github/subjekt/SubjektCompiler.kt
@@ -1,14 +1,38 @@
 package io.github.subjekt
 
+import io.github.subjekt.nodes.suite.Suite
+import io.github.subjekt.resolved.ResolvedSuite
+import io.github.subjekt.utils.MessageCollector
+import io.github.subjekt.visitors.SuiteVisitor
+import io.github.subjekt.yaml.Reader.suiteFromResource
+import io.github.subjekt.yaml.Reader.suiteFromYaml
+import org.intellij.lang.annotations.Language
 import java.io.File
 
 object SubjektCompiler {
 
-  fun compile(code: String) {
-    TODO()
+  private fun io.github.subjekt.yaml.Suite?.resolve(messageCollector: MessageCollector): ResolvedSuite? {
+    if (this == null) {
+      return null
+    }
+    val suite = Suite.fromYamlSuite(this)
+    val visitor = SuiteVisitor(messageCollector)
+    visitor.visitSuite(suite)
+    return ResolvedSuite(suite.name, visitor.resolvedSubjects.toSet(), suite.configuration)
   }
 
-  fun compile(file: File) {
-    TODO()
-  }
+  fun compile(
+    @Language("yaml") code: String,
+    messageCollector: MessageCollector = MessageCollector.SimpleCollector(),
+  ): ResolvedSuite? = suiteFromYaml(code, messageCollector).resolve(messageCollector)
+
+  fun compile(
+    file: File,
+    messageCollector: MessageCollector = MessageCollector.SimpleCollector(),
+  ): ResolvedSuite? = suiteFromYaml(file, messageCollector).resolve(messageCollector)
+
+  fun compileResource(
+    resource: String,
+    messageCollector: MessageCollector = MessageCollector.SimpleCollector(),
+  ): ResolvedSuite? = suiteFromResource(resource, messageCollector).resolve(messageCollector)
 }
--- compiler/src/main/kotlin/io/github/subjekt/resolved/Resolved.kt
+++ compiler/src/main/kotlin/io/github/subjekt/resolved/Resolved.kt
@@ -1,5 +1,7 @@
 package io.github.subjekt.resolved
 
+import io.github.subjekt.yaml.Configuration
+
 sealed class ResolvedOutcome(open val message: String) {
   data class Warning(override val message: String) : ResolvedOutcome(message)
   data class Error(override val message: String) : ResolvedOutcome(message)
@@ -9,4 +11,10 @@ data class ResolvedSubject(
   val name: String,
   val code: String,
   val outcomes: List<ResolvedOutcome>,
+)
+
+data class ResolvedSuite(
+  val name: String,
+  val subjects: Set<ResolvedSubject>,
+  val configuration: Configuration,
 )
