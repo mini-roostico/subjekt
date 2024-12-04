abc22b96cdd1d461dfb5c1f03730202c05fa318d
FreshMag <pescomagnani@gmail.com>
2024-12-04 18:33:31 +0100

    fix: fixed bugged 'idFromCode'

--- compiler/src/main/kotlin/io/github/subjekt/conversion/Stdlib.kt
+++ compiler/src/main/kotlin/io/github/subjekt/conversion/Stdlib.kt
@@ -19,11 +19,18 @@ object Stdlib {
   }
 
   private fun String.substringUntilFirstInvalidChar(): String {
-    val startIndex = indexOfFirst { !it.isLetter() }
-    return if (startIndex != -1) substring(0, startIndex) else ""
+    val endIndex = indexOfFirst { !it.isLetter() }
+    return if (endIndex != -1) substring(0, endIndex) else this
   }
 
-  private fun idFromCode(code: String, maxLength: Int = 20): String = code.substringStartingFromFirstValidChar().trim().substringUntilFirstInvalidChar().trim().run {
-    substring(0, min(maxLength, length))
-  }.replace("[^a-zA-Z0-9]".toRegex(), "").replaceFirstChar(Char::titlecase)
+  private fun idFromCode(code: String, maxLength: Int = 20): String = code
+    .substringStartingFromFirstValidChar()
+    .trim()
+    .substringUntilFirstInvalidChar()
+    .trim()
+    .run {
+      substring(0, min(maxLength, length))
+    }
+    .replace("[^a-zA-Z0-9]".toRegex(), "")
+    .replaceFirstChar(Char::titlecase)
 }
