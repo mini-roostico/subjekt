2ece354d0592f1dc490e1495514ef39b87c5be59
FreshMag <pescomagnani@gmail.com>
2024-12-04 18:39:12 +0100

    docs: added documentation in the conversion package

--- compiler/src/main/kotlin/io/github/subjekt/conversion/Stdlib.kt
+++ compiler/src/main/kotlin/io/github/subjekt/conversion/Stdlib.kt
@@ -2,16 +2,26 @@ package io.github.subjekt.conversion
 
 import kotlin.math.min
 
+/**
+ * Object containing standard library functions that can be used as macros.
+ */
 @SubjektModule("std")
 object Stdlib {
 
+  /**
+   * Capitalizes the first letter of a string.
+   */
   @JvmStatic
   @Macro("capitalizeFirst")
   fun capitalizeFirst(str: String): List<String> = listOf(str.replaceFirstChar(Char::titlecase))
 
+  /**
+   * Prettifies [arguments] by removing all non-alphanumeric characters and joining them together following a Pascal case
+   * notation.
+   */
   @JvmStatic
   @Macro("prettify")
-  fun prettify(vararg parametersValues: String): List<String> = listOf(parametersValues.joinToString("") { idFromCode(it) })
+  fun prettify(vararg arguments: String): List<String> = listOf(arguments.joinToString("") { idFromCode(it) })
 
   private fun String.substringStartingFromFirstValidChar(): String {
     val startIndex = indexOfFirst { it.isLetter() }
@@ -23,6 +33,9 @@ object Stdlib {
     return if (endIndex != -1) substring(0, endIndex) else this
   }
 
+  /**
+   * Obtains an identifier from a code snippet.
+   */
   private fun idFromCode(code: String, maxLength: Int = 20): String = code
     .substringStartingFromFirstValidChar()
     .trim()
--- compiler/src/main/kotlin/io/github/subjekt/conversion/SubjektModule.kt
+++ compiler/src/main/kotlin/io/github/subjekt/conversion/SubjektModule.kt
@@ -1,5 +1,8 @@
 package io.github.subjekt.conversion
 
+/**
+ * Annotation used to mark a Kotlin object as a Subjekt module.
+ */
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
 annotation class SubjektModule(val name: String)
