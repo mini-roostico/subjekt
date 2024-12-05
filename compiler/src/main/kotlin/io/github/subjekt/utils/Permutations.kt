a296296719562c2d25115ea41d9e5dfe73ff2758
FreshMag <pescomagnani@gmail.com>
2024-12-05 11:24:53 +0100

    docs: added documentation to resolved and utils packages

--- compiler/src/main/kotlin/io/github/subjekt/utils/Permutations.kt
+++ compiler/src/main/kotlin/io/github/subjekt/utils/Permutations.kt
@@ -1,15 +1,18 @@
 package io.github.subjekt.utils
 
 import io.github.subjekt.nodes.suite.Parameter
+import io.github.subjekt.resolved.ResolvedParameter
 
+/**
+ * Utility object for generating permutations.
+ */
 object Permutations {
 
-  data class DefinedParameter(
-    val identifier: String,
-    val value: Any,
-  )
-
-  fun List<Parameter>.permute(parameterConfigurationConsumer: (List<DefinedParameter>) -> Unit) {
+  /**
+   * Generates all possible permutations of the given [List] of [Parameter]s and calls the [parameterConfigurationConsumer]
+   * for each permutation.
+   */
+  fun List<Parameter>.permute(parameterConfigurationConsumer: (List<ResolvedParameter>) -> Unit) {
     val cartesianProduct = this.map { it.values }.fold(sequenceOf(emptyList<Any>())) { acc, values ->
       acc.flatMap { combination ->
         values.asSequence().map { combination + it }
@@ -18,19 +21,21 @@ object Permutations {
 
     cartesianProduct.map { combination ->
       combination.mapIndexed { index, value ->
-        DefinedParameter(this[index].name, value)
+        ResolvedParameter(this[index].name, value)
       }
     }.forEach {
       parameterConfigurationConsumer(it)
     }
   }
 
-  fun <T> Iterable<Iterable<T>>.permute(): Iterable<Iterable<T>> {
-    return fold(listOf(emptyList<T>()) as Iterable<List<T>>) { acc, iterable ->
-      acc.flatMap { combination ->
-        iterable.map { element ->
-          combination + element
-        }
+  /**
+   * Generates all possible permutations of the given [List] of [List]s and returns them.
+   * For example: `[["a", "b"], ["1", "2"]]` will return `[["a", "1"], ["a", "2"], ["b", "1"], ["b", "2"]]`.
+   */
+  fun <T> Iterable<Iterable<T>>.permute(): Iterable<Iterable<T>> = fold(listOf(emptyList<T>()) as Iterable<List<T>>) { acc, iterable ->
+    acc.flatMap { combination ->
+      iterable.map { element ->
+        combination + element
       }
     }
   }
