ad4a4148616d12f502d0098afc91746d7b22071d
FreshMag <pescomagnani@gmail.com>
2024-12-05 11:37:28 +0100

    docs: added documentation to SubjektCompiler.kt

--- compiler/src/main/kotlin/io/github/subjekt/SubjektCompiler.kt
+++ compiler/src/main/kotlin/io/github/subjekt/SubjektCompiler.kt
@@ -10,8 +10,16 @@ import io.github.subjekt.yaml.Reader.suiteFromYaml
 import org.intellij.lang.annotations.Language
 import java.io.File
 
+/**
+ * Entry point for the compiler of Subjekt YAML suites. It provides methods to compile YAML code, files, and resources
+ * into [ResolvedSuite]s.
+ */
 object SubjektCompiler {
 
+  /**
+   * Resolves a nullable [Suite] into a [ResolvedSuite] using the Subjekt compiler. It returns null if the suite is null
+   * or if an error occurred during the resolution.
+   */
   private fun io.github.subjekt.yaml.Suite?.resolve(messageCollector: MessageCollector): ResolvedSuite? {
     if (this == null) {
       return null
@@ -22,16 +30,25 @@ object SubjektCompiler {
     return ResolvedSuite(suite.name, visitor.resolvedSubjects.toSet(), suite.configuration)
   }
 
+  /**
+   * Compiles a YAML [code] string into a [ResolvedSuite]. It returns null if an error occurred during the compilation.
+   */
   fun compile(
     @Language("yaml") code: String,
     messageCollector: MessageCollector = MessageCollector.SimpleCollector(),
   ): ResolvedSuite? = suiteFromYaml(code, messageCollector).resolve(messageCollector)
 
+  /**
+   * Compiles a YAML [file] into a [ResolvedSuite]. It returns null if an error occurred during the compilation.
+   */
   fun compile(
     file: File,
     messageCollector: MessageCollector = MessageCollector.SimpleCollector(),
   ): ResolvedSuite? = suiteFromYaml(file, messageCollector).resolve(messageCollector)
 
+  /**
+   * Compiles a YAML resource [resource] into a [ResolvedSuite]. It returns null if an error occurred during the compilation.
+   */
   fun compileResource(
     resource: String,
     messageCollector: MessageCollector = MessageCollector.SimpleCollector(),
