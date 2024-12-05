601333061c4b4d5aee371aa0bade19b193a9923d
FreshMag <pescomagnani@gmail.com>
2024-12-05 11:00:05 +0100

    docs: added documentation in the nodes.suite package

--- compiler/src/main/kotlin/io/github/subjekt/nodes/suite/Macro.kt
+++ compiler/src/main/kotlin/io/github/subjekt/nodes/suite/Macro.kt
@@ -3,16 +3,33 @@ package io.github.subjekt.nodes.suite
 import io.github.subjekt.resolved.Resolvable
 import io.github.subjekt.yaml.Configuration
 
+/**
+ * Represents a macro definition node.
+ */
 class Macro(
+  /**
+   * The identifier of the macro.
+   */
   val identifier: String,
+  /**
+   * The list of arguments identifiers.
+   */
   val argumentsIdentifiers: List<String>,
+  /**
+   * The list of bodies of the macro. Each macro can have multiple, resolvable bodies.
+   */
   val bodies: List<Resolvable>,
 ) {
-
+  /**
+   * The number of arguments the macro expects.
+   */
   val argumentsNumber: Int
     get() = argumentsIdentifiers.size
 
   companion object {
+    /**
+     * Creates a Macro node from a YAML [macro] parsed data class. The [config] is used to parse the bodies.
+     */
     fun fromYamlMacro(macro: io.github.subjekt.yaml.Macro, config: Configuration): Macro {
       if (!macro.def.contains("(")) {
         throw IllegalArgumentException("Illegal macro definition. Expected '(' in ${macro.def}")
