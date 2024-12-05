5e036cda921d879d2149ecde7e5c13a683f06152
FreshMag <pescomagnani@gmail.com>
2024-12-05 11:31:55 +0100

    docs: added documentation to the visitors package

--- compiler/src/main/kotlin/io/github/subjekt/visitors/SuiteIrVisitor.kt
+++ compiler/src/main/kotlin/io/github/subjekt/visitors/SuiteIrVisitor.kt
@@ -7,11 +7,38 @@ import io.github.subjekt.nodes.suite.Subject
 import io.github.subjekt.nodes.suite.Suite
 import io.github.subjekt.nodes.suite.Template
 
+/**
+ * Visitor for the intermediate representation of a suite. Unlike the [io.github.subjekt.nodes.expression.Node]
+ * representation, this visitor visits the nodes specified in the [io.github.subjekt.nodes.suite] package.
+ */
 interface SuiteIrVisitor<T> {
+  /**
+   * Visits a [Suite].
+   */
   fun visitSuite(suite: Suite): T
+
+  /**
+   * Visits a [Subject].
+   */
   fun visitSubject(subject: Subject): T
+
+  /**
+   * Visits a [Parameter].
+   */
   fun visitParameter(parameter: Parameter): T
+
+  /**
+   * Visits an [Outcome].
+   */
   fun visitOutcome(outcome: Outcome): T
+
+  /**
+   * Visits a [Macro].
+   */
   fun visitMacro(macro: Macro): T
+
+  /**
+   * Visits a [Template].
+   */
   fun visitTemplate(template: Template): T
 }
