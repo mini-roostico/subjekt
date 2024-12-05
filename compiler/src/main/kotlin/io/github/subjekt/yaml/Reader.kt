b72d1bbeef3009e7742f85c65e79b4d819f178e4
FreshMag <pescomagnani@gmail.com>
2024-12-05 11:35:21 +0100

    docs: added documentation to the yaml package

--- compiler/src/main/kotlin/io/github/subjekt/yaml/Reader.kt
+++ compiler/src/main/kotlin/io/github/subjekt/yaml/Reader.kt
@@ -7,19 +7,30 @@ import io.github.subjekt.nodes.Context
 import io.github.subjekt.utils.MessageCollector
 import java.io.File
 
+/**
+ * Reader for YAML files.
+ */
 object Reader {
 
   private val mapper = ObjectMapper(YAMLFactory()).apply {
     findAndRegisterModules()
   }
 
-  fun suiteFromYaml(path: File, messageCollector: MessageCollector = MessageCollector.SimpleCollector()): Suite? = try {
-    mapper.readValue(path)
+  /**
+   * Parse a YAML [file] into a [Suite] object. It reports errors to the [messageCollector]. Returns the [Suite] object
+   * parsed from the YAML file, or null if an error occurred.
+   */
+  fun suiteFromYaml(file: File, messageCollector: MessageCollector = MessageCollector.SimpleCollector()): Suite? = try {
+    mapper.readValue(file)
   } catch (e: Exception) {
-    messageCollector.error("Failed to parse YAML file: $path. Error: ${e.message}", Context.emptyContext(), -1)
+    messageCollector.error("Failed to parse YAML file: $file. Error: ${e.message}", Context.emptyContext(), -1)
     null
   }
 
+  /**
+   * Parse a YAML [yaml] string into a [Suite] object. It reports errors to the [messageCollector]. Returns the [Suite]
+   * object parsed from the YAML string, or null if an error occurred.
+   */
   fun suiteFromYaml(yaml: String, messageCollector: MessageCollector = MessageCollector.SimpleCollector()): Suite? = try {
     mapper.readValue(yaml)
   } catch (e: Exception) {
@@ -27,6 +38,10 @@ object Reader {
     null
   }
 
+  /**
+   * Parse a YAML resource [path] into a [Suite] object. It reports errors to the [messageCollector]. Returns the [Suite]
+   * object parsed from the YAML resource, or null if an error occurred.
+   */
   fun suiteFromResource(path: String, messageCollector: MessageCollector = MessageCollector.SimpleCollector()): Suite? {
     object {}::class.java.classLoader.getResourceAsStream(path)?.bufferedReader().use {
       if (it == null) {
--- compiler/src/main/kotlin/io/github/subjekt/yaml/SerializableSuite.kt
+++ compiler/src/main/kotlin/io/github/subjekt/yaml/SerializableSuite.kt
@@ -1,5 +1,8 @@
 package io.github.subjekt.yaml
 
+/**
+ * Serializable representation of a suite. It can be converted to a [io.github.subjekt.nodes.suite.Suite] node.
+ */
 data class Suite(
   val name: String,
   val config: Configuration?,
@@ -8,6 +11,9 @@ data class Suite(
   val parameters: List<Parameter>?,
 )
 
+/**
+ * Serializable representation of a subject. It can be converted to a [io.github.subjekt.nodes.suite.Subject] node.
+ */
 data class Subject(
   val name: String,
   val parameters: List<Parameter>?,
@@ -16,12 +22,30 @@ data class Subject(
   val outcomes: List<Outcome>,
 )
 
-data class Macro(val def: String, val values: List<String>)
+/**
+ * Serializable representation of a macro. It can be converted to a [io.github.subjekt.nodes.suite.Macro] node.
+ */
+data class Macro(
+  /**
+   * Textual definition of the macro in the form `macroName(param1, param2, ...)`.
+   */
+  val def: String,
+  val values: List<String>,
+)
 
+/**
+ * Serializable representation of a parameter. It can be converted to a [io.github.subjekt.nodes.suite.Parameter] node.
+ */
 data class Parameter(val name: String, val values: List<Any>)
 
+/**
+ * Serializable representation of an outcome. It can be converted to a [io.github.subjekt.nodes.suite.Outcome] node.
+ */
 data class Outcome(val warning: String?, val error: String?)
 
+/**
+ * Serializable representation of the configuration of a suite. It also stores the default values for the configuration.
+ */
 data class Configuration(
   val codePreamble: String = "",
   val expressionPrefix: String = "\${{",
