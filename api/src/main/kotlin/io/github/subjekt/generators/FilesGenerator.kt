eae946e5417ef4ffef6bf7b64c6bcaab24274feb
FreshMag <pescomagnani@gmail.com>
2024-12-12 11:50:59 +0100

    feat: added more generators (temp files and plain subjects)

--- api/src/main/kotlin/io/github/subjekt/generators/FilesGenerator.kt
+++ api/src/main/kotlin/io/github/subjekt/generators/FilesGenerator.kt
@@ -3,6 +3,7 @@ package io.github.subjekt.generators
 import io.github.subjekt.Subjekt
 import io.github.subjekt.dsl.SubjektContext
 import io.github.subjekt.files.Utils.cleanName
+import io.github.subjekt.generators.SubjectGenerator.toResolvedSubjects
 import io.github.subjekt.linting.Linter
 import java.io.File
 
@@ -17,21 +18,25 @@ object FilesGenerator {
   fun SubjektContext.toFiles(path: String, extension: String) {
     val dir = File(path)
     dir.mkdirs()
-    getSources().forEach { source ->
-      val generatedSubjects = source.getGeneratedSubjects()
-      generatedSubjects.forEach {
-        var fileName = cleanName(it.name)
-        var file = File("$path/$fileName.$extension")
-        if (file.exists()) {
-          file.delete()
-        }
-        val preamble = source.configuration.codePreamble
-        var code = preamble + "\n" + it.code
-        if (source.configuration.lint) {
-          code = Linter.lint(code, Subjekt.reporter)
-        }
-        file.writeText(code)
+    this.toResolvedSubjects().forEach { subject ->
+      var fileName = cleanName(subject.name)
+      var file = File("$path/$fileName.$extension")
+      if (file.exists()) {
+        file.delete()
       }
+      file.writeText(subject.code)
     }
   }
+
+  /**
+   * Generates temporary files from a SubjektContext, associating each file with its cleaned name
+   */
+  fun SubjektContext.toTempFiles(): Map<String, File> =
+    this.toResolvedSubjects().associate { subject ->
+      var fileName = cleanName(subject.name)
+      var file = File.createTempFile(fileName, ".kt")
+      file.writeText(subject.code)
+      fileName to file
+    }
+
 }
