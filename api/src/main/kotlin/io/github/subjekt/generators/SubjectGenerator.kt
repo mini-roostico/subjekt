08c0c4f3250d715bbb2e55f7314ac44420bfbbda
FreshMag <pescomagnani@gmail.com>
2024-12-16 18:07:35 +0100

    style: fixed style

--- api/src/main/kotlin/io/github/subjekt/generators/SubjectGenerator.kt
+++ api/src/main/kotlin/io/github/subjekt/generators/SubjectGenerator.kt
@@ -10,20 +10,20 @@ import io.github.subjekt.resolved.ResolvedSubject
  */
 object SubjectGenerator {
 
-    /**
-     * Resolves the subjects from a SubjektContext and returns them as a set of ResolvedSubjects, applying linting if
-     * necessary and adding the preamble to the code
-     */
-    fun SubjektContext.toResolvedSubjects(): Set<ResolvedSubject> {
-        return getSources().flatMap { source ->
-            source.getGeneratedSubjects().map {
-                val preamble = source.configuration.codePreamble
-                var code = (if (preamble.isNotBlank()) preamble + "\n" else "") + it.code
-                if (source.configuration.lint) {
-                    code = Linter.lint(code, Subjekt.reporter)
-                }
-                ResolvedSubject(it.name, code, it.outcomes)
-            }
-        }.toSet()
-    }
+  /**
+   * Resolves the subjects from a SubjektContext and returns them as a set of ResolvedSubjects, applying linting if
+   * necessary and adding the preamble to the code
+   */
+  fun SubjektContext.toResolvedSubjects(): Set<ResolvedSubject> {
+    return getSources().flatMap { source ->
+      source.getGeneratedSubjects().map {
+        val preamble = source.configuration.codePreamble
+        var code = (if (preamble.isNotBlank()) preamble + "\n" else "") + it.code
+        if (source.configuration.lint) {
+          code = Linter.lint(code, Subjekt.reporter)
+        }
+        ResolvedSubject(it.name, code, it.outcomes)
+      }
+    }.toSet()
+  }
 }
