2de3dcdfa55e9f3f02e258a0ffc5bb3c15435760
FreshMag <pescomagnani@gmail.com>
2025-01-13 11:50:02 +0100

    feat: added base classes for new `core` package

---
+++ src/commonMain/kotlin/io/github/subjekt/core/resolution/Instance.kt
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2024, Francesco Magnani, Luca Rubboli,
+ * and all authors listed in the `build.gradle.kts` and the generated `pom.xml` file.
+ *
+ *  This file is part of Subjekt, and is distributed under the terms of the Apache License 2.0, as described in the
+ *  LICENSE file in this project's repository's top directory.
+ *
+ */
+
+package io.github.subjekt.core.resolution
+
+class Instance
