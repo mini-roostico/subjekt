971b5fa4303c415f6a64e9b74ca0137fd003c47e
FreshMag <pescomagnani@gmail.com>
2025-01-14 22:08:25 +0100

    feat: added Configuration.kt

--- src/commonMain/kotlin/io/github/subjekt/core/Configuration.kt
+++ src/commonMain/kotlin/io/github/subjekt/core/Configuration.kt
@@ -14,23 +14,109 @@ class Configuration : MutableMap<String, Any> by mutableMapOf<String, Any>() {
      * The code preamble of the suite.
      */
     val codePreamble: String
-        get() = this["codePreamble"] as? String ?: ""
+        get() = this[DEFAULT_CODE_PREAMBLE_KEY] as? String ?: DEFAULT_CODE_PREAMBLE
 
     /**
      * Prefix used to identify the start of an expression.
      */
     val expressionPrefix: String
-        get() = this["expressionPrefix"] as? String ?: "\${{"
+        get() = this[DEFAULT_EXPRESSION_PREFIX_KEY] as? String ?: DEFAULT_EXPRESSION_PREFIX
 
     /**
      * Suffix used to identify the end of an expression.
      */
     val expressionSuffix: String
-        get() = this["expressionSuffix"] as? String ?: "}}"
+        get() = this[DEFAULT_EXPRESSION_SUFFIX_KEY] as? String ?: DEFAULT_EXPRESSION_SUFFIX
 
     /**
      * Whether to lint the suite or not.
      */
     val lint: Boolean
-        get() = (this["lint"] as? String)?.toBooleanStrictOrNull() != false // Default to true
+        get() = (this[DEFAULT_LINT_KEY] as? String)?.toBooleanStrictOrNull() == !DEFAULT_LINT
+
+    /**
+     * Sets the value of the given key. Returns `true` if the key is among the default ones, `false` if it's a custom
+     * key instead.
+     */
+    fun set(
+        key: String,
+        value: Any,
+    ): Boolean {
+        when (key) {
+            in CODE_PREAMBLE_KEYS -> this[DEFAULT_CODE_PREAMBLE_KEY] = value
+            in EXPRESSION_PREFIX_KEYS -> this[DEFAULT_EXPRESSION_PREFIX_KEY] = value
+            in EXPRESSION_SUFFIX_KEYS -> this[DEFAULT_EXPRESSION_SUFFIX_KEY] = value
+            in LINT_KEYS -> this[DEFAULT_LINT_KEY] = value
+            else -> return false
+        }
+        return true
+    }
+
+    /**
+     * Companion object used to define default values.
+     */
+    companion object {
+        private const val DEFAULT_CODE_PREAMBLE_KEY = "codePreamble"
+
+        /**
+         * Default code preamble.
+         */
+        internal const val DEFAULT_CODE_PREAMBLE = ""
+
+        /**
+         * Keys used to identify the code preamble.
+         */
+        internal val CODE_PREAMBLE_KEYS = setOf(DEFAULT_CODE_PREAMBLE_KEY, "code_preamble", "code-preamble", "preamble")
+
+        private const val DEFAULT_EXPRESSION_PREFIX_KEY = "expressionPrefix"
+
+        /**
+         * Default expression prefix.
+         */
+        internal const val DEFAULT_EXPRESSION_PREFIX = "\${{"
+
+        /**
+         * Keys used to identify the expression prefix.
+         */
+        internal val EXPRESSION_PREFIX_KEYS =
+            setOf(DEFAULT_EXPRESSION_PREFIX_KEY, "expression_prefix", "expression-prefix", "prefix")
+
+        private const val DEFAULT_EXPRESSION_SUFFIX_KEY = "expressionSuffix"
+
+        /**
+         * Default expression suffix.
+         */
+        internal const val DEFAULT_EXPRESSION_SUFFIX = "}}"
+
+        /**
+         * Keys used to identify the expression suffix.
+         */
+        internal val EXPRESSION_SUFFIX_KEYS =
+            setOf(DEFAULT_EXPRESSION_SUFFIX_KEY, "expression_suffix", "expression-suffix", "suffix")
+
+        private const val DEFAULT_LINT_KEY = "lint"
+
+        /**
+         * Default value for linting.
+         */
+        internal const val DEFAULT_LINT = false
+
+        /**
+         * Keys used to identify whether to lint the suite or not.
+         */
+        internal val LINT_KEYS =
+            setOf(
+                DEFAULT_LINT_KEY,
+                "linting",
+                "enableLinting",
+                "enable_linting",
+                "enable-linting",
+                "lintingOn",
+                "lintOn",
+                "lint-on",
+                "linting-on",
+                "linting_on",
+                "lint_on",
+            )
+    }
 }
