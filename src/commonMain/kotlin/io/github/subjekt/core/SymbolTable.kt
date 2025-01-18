79312daea7f93ee58ec0542f0774cf872faf6bc5
FreshMag <pescomagnani@gmail.com>
2025-01-18 14:57:19 +0100

    refactor: changed macro name resolution inside SymbolTable making overloading possible

--- src/commonMain/kotlin/io/github/subjekt/core/SymbolTable.kt
+++ src/commonMain/kotlin/io/github/subjekt/core/SymbolTable.kt
@@ -12,80 +12,116 @@ package io.github.subjekt.core
 /**
  * Represents a collection of [Parameter]s, [Macro]s, and [Function1]s that can be used to resolve values in a Suite.
  */
-data class SymbolTable(
-    /**
-     * The parameters that can be used to resolve values in a Suite.
-     */
-    val parameters: Map<String, Parameter> = emptyMap(),
-    /**
-     * The macros that can be used to resolve values in a Suite.
-     */
-    val macros: Map<String, Macro> = emptyMap(),
-    /**
-     * The functions that can be used to resolve values in a Suite.
-     *
-     * **Important**: Functions can be defined only programmatically, and are not part of the Suite definition.
-     */
-    val functions: Map<String, Function1<List<*>, List<*>>> = emptyMap(),
-) {
-    /**
-     * Returns a new [SymbolTable] with both the symbols from this SymbolTable and the symbols from [symbolTable].
-     */
-    operator fun plus(symbolTable: SymbolTable): SymbolTable =
-        SymbolTable(
-            parameters + symbolTable.parameters,
-            macros + symbolTable.macros,
-            functions + symbolTable.functions,
-        )
+@ConsistentCopyVisibility
+data class SymbolTable
+    internal constructor(
+        /**
+         * The parameters that can be used to resolve values in a Suite.
+         */
+        val parameters: Map<String, Parameter> = emptyMap(),
+        /**
+         * The macros that can be used to resolve values in a Suite.
+         */
+        val macros: Map<String, Macro> = emptyMap(),
+        /**
+         * The functions that can be used to resolve values in a Suite.
+         *
+         * **Important**: Functions can be defined only programmatically, and are not part of the Suite definition.
+         */
+        val functions: Map<String, Function1<List<*>, List<*>>> = emptyMap(),
+    ) {
+        /**
+         * Returns a new [SymbolTable] with both the symbols from this SymbolTable and the symbols from [symbolTable].
+         */
+        operator fun plus(symbolTable: SymbolTable): SymbolTable =
+            SymbolTable(
+                parameters + symbolTable.parameters,
+                macros + symbolTable.macros,
+                functions + symbolTable.functions,
+            )
 
-    /**
-     * Returns a new [SymbolTable] with the [Parameter] [parameter] defined.
-     */
-    fun defineParameter(parameter: Parameter): SymbolTable = copy(parameters = parameters + (parameter.id to parameter))
+        /**
+         * Returns a new [SymbolTable] with the [Parameter] [parameter] defined.
+         */
+        fun defineParameter(parameter: Parameter): SymbolTable =
+            copy(
+                parameters =
+                    parameters + (parameter.id to parameter),
+            )
 
-    /**
-     * Returns a new [SymbolTable] with the [Parameter]s [parameters] defined.
-     */
-    fun defineParameters(parameters: Iterable<Parameter>): SymbolTable =
-        copy(
-            parameters =
-                this.parameters + parameters.map { (it.id to it) },
-        )
+        /**
+         * Returns a new [SymbolTable] with the [Parameter]s [parameters] defined.
+         */
+        fun defineParameters(parameters: Iterable<Parameter>): SymbolTable =
+            copy(
+                parameters =
+                    this.parameters + parameters.map { (it.id to it) },
+            )
 
-    /**
-     * Returns a new [SymbolTable] with the [Macro] [macro] defined.
-     */
-    fun defineMacro(macro: Macro): SymbolTable = copy(macros = macros + (macro.id to macro))
+        /**
+         * Returns a new [SymbolTable] with the [Macro] [macro] defined.
+         */
+        fun defineMacro(macro: Macro): SymbolTable =
+            copy(
+                macros =
+                    macros + ((macro.id + ARGS_SEPARATOR + macro.argumentsIdentifiers.size) to macro),
+            )
 
-    /**
-     * Returns a new [SymbolTable] with the [Macro]s [macros] defined.
-     */
-    fun defineMacros(macros: Iterable<Macro>): SymbolTable = copy(macros = this.macros + macros.map { (it.id to it) })
+        /**
+         * Returns a new [SymbolTable] with the [Macro]s [macros] defined.
+         */
+        fun defineMacros(macros: Iterable<Macro>): SymbolTable =
+            copy(
+                macros =
+                    this.macros + macros.map { ((it.id + ARGS_SEPARATOR + it.argumentsIdentifiers.size) to it) },
+            )
 
-    /**
-     * Returns a new [SymbolTable] with the [Function1] [function] defined.
-     */
-    fun defineFunction(
-        id: String,
-        function: Function1<List<*>, List<*>>,
-    ): SymbolTable =
-        copy(
-            functions =
-                functions + (id to function),
-        )
+        /**
+         * Returns a new [SymbolTable] with the [Function1] [function] defined.
+         */
+        fun defineFunction(
+            id: String,
+            function: Function1<List<*>, List<*>>,
+        ): SymbolTable =
+            copy(
+                functions =
+                    functions + (id to function),
+            )
 
-    /**
-     * Returns the [Parameter] associated to [id], or `null` if no such parameter can be resolved.
-     */
-    fun resolveParameter(id: String): Parameter? = parameters[id]
+        /**
+         * Returns the [Parameter] associated to [id], or `null` if no such parameter can be resolved.
+         */
+        fun resolveParameter(id: String): Parameter? = parameters[id]
 
-    /**
-     * Returns the [Macro] associated to [id], or `null` if no such macro can be resolved.
-     */
-    fun resolveMacro(id: String): Macro? = macros[id]
+        /**
+         * Returns the [Macro] associated to [id], or `null` if no such macro can be resolved. Since Subjekt supports
+         * a light overloading system, you must specify the [argsNumber] the macro has to correctly resolve it.
+         */
+        fun resolveMacro(
+            id: String,
+            argsNumber: Int = 0,
+        ): Macro? = macros[id + ARGS_SEPARATOR + argsNumber]
 
-    /**
-     * Returns the [Function1] associated to [id], or `null` if no such function can be resolved.
-     */
-    fun resolveFunction(id: String): Function1<List<*>, List<*>>? = functions[id]
-}
+        /**
+         * Returns the [Macro]s associated to [id], or an empty list if no such macro can be resolved. This includes all
+         * the overloaded versions of the macro.
+         */
+        fun resolveMacros(id: String): List<Macro> =
+            macros
+                .filterKeys {
+                    it.substring(it.indexOfFirst { it.isDigit() } - 1) == id
+                }.values
+                .toList()
+
+        /**
+         * Returns the [Function1] associated to [id], or `null` if no such function can be resolved.
+         */
+        fun resolveFunction(id: String): Function1<List<*>, List<*>>? = functions[id]
+
+        companion object {
+            /**
+             * The separator used to separate the arguments of a macro during the ID resolution.
+             */
+            private const val ARGS_SEPARATOR = ""
+        }
+    }
