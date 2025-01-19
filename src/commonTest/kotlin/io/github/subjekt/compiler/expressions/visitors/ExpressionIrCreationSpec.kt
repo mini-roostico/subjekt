0007cfd1695995afe48a1532919b07357ac05c68
FreshMag <pescomagnani@gmail.com>
2025-01-19 15:03:12 +0100

    refactor: created IrTree wrapper for Ir parsing result

--- src/commonTest/kotlin/io/github/subjekt/compiler/expressions/visitors/ExpressionIrCreationSpec.kt
+++ src/commonTest/kotlin/io/github/subjekt/compiler/expressions/visitors/ExpressionIrCreationSpec.kt
@@ -10,7 +10,12 @@
 package io.github.subjekt.compiler.expressions.visitors
 
 import io.github.subjekt.compiler.expressions.Expression
-import io.github.subjekt.compiler.expressions.ir.IrNode
+import io.github.subjekt.compiler.expressions.ir.IrNode.IrCall
+import io.github.subjekt.compiler.expressions.ir.IrNode.IrDotCall
+import io.github.subjekt.compiler.expressions.ir.IrNode.IrExpressionPlus
+import io.github.subjekt.compiler.expressions.ir.IrNode.IrLiteral
+import io.github.subjekt.compiler.expressions.ir.IrNode.IrParameter
+import io.github.subjekt.compiler.expressions.ir.IrNode.IrTree
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 
@@ -19,31 +24,31 @@ class ExpressionIrCreationSpec : StringSpec({
         val expr = Expression("variable")
         val result = expr.parseToIr()
 
-        result shouldBe IrNode.IrParameter("variable", 1)
+        result shouldBe IrTree(IrParameter("variable", 1))
     }
 
     "visitPlusExpr should return correct IrExpressionPlus" {
         val expr = Expression("a + b")
         val result = expr.parseToIr()
 
-        result shouldBe IrNode.IrExpressionPlus(IrNode.IrParameter("a", 1), IrNode.IrParameter("b", 1), 1)
+        result shouldBe IrTree(IrExpressionPlus(IrParameter("a", 1), IrParameter("b", 1), 1))
     }
 
     "visitLiteral should return correct IrLiteral" {
         val expr = Expression("\"literal\"")
         val result = expr.parseToIr()
-        result shouldBe IrNode.IrLiteral("literal", 1)
+        result shouldBe IrTree(IrLiteral("literal", 1))
     }
 
     "visitMacroCall should return correct IrCall" {
         val expr = Expression("macro(arg)")
         val result = expr.parseToIr()
-        result shouldBe IrNode.IrCall("macro", listOf(IrNode.IrParameter("arg", 1)), 1)
+        result shouldBe IrTree(IrCall("macro", listOf(IrParameter("arg", 1)), 1))
     }
 
     "visitDotCall should return correct IrDotCall" {
         val expr = Expression("module.macro(arg)")
         val result = expr.parseToIr()
-        result shouldBe IrNode.IrDotCall("module", "macro", listOf(IrNode.IrParameter("arg", 1)), 1)
+        result shouldBe IrTree(IrDotCall("module", "macro", listOf(IrParameter("arg", 1)), 1))
     }
 })
